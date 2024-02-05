22bec97a229f7a55298274c8f3274ebecb0214f9
lccsam1003 <77393786+lccsam1003@users.noreply.github.com>
2024-02-05 19:57:38 +0800

    Create Exam1.py

---
+++ Exam1.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+class Geometric():
+    
+    def __init__(self):
+        self.color = "Green"
+        
+class Circle():
+    
+    def __init__(self,radius):
+        super().__init__()
+        self.PI = 3.14159
+        self.radius = radius
+    def getRadius(self):
+        return self.radius
+    def setRadius(self,radius):
+        self.radius = radius
+    def getDiameter(self):
+        return self.radius * 2
+    def getPerimeter(self):
+        return self.radius * 2 * self.PI
+    def getArea(self):
+        return self.PI * (self.radius ** 2)
+
+
+A = Circle(5)
+
+print("圓形的半徑 : ", A.getRadius())
+print("圓形的直徑 : ", A.getDiameter())
+print("圓形的圓周 : ", A.getPerimeter())
+print("圓形的面積 : ", A.getArea())
+A.setRadius(10)
+print("圓形的直徑 : ", A.getDiameter())
+
+
+
+
+
+                       
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
